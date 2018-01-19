f13c30624ec9ed9ee5c281baaa51e7e63005be8c
cwl48 <1159849884@qq.com>
2018-01-19 18:43:52 +0800

    添加路由配置

--- src/common/router.js
+++ src/common/router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Login from "../pages/Login";
 import Editor from "../pages/Editor";
 import {Redirect, Route} from "react-router-dom";
-
+import {renderRoutes} from "react-router-config"
 
 export const routes = [
     {
@@ -10,35 +10,41 @@ export const routes = [
         component: Login
     },
     {
-        path: "/editor/drafts",
-        component: Editor
-    },
-    {
-        path: "/editor/draft/new",
-        component: Editor
-    },
-    {
-        path: '/editor/draft/:id',
-        component: Editor
+        path: "/editor",
+        component:Editor,
+        routes: [
+            {
+                path: "drafts",
+                component: Editor
+            },
+            {
+                path: "draft/new",
+                component: Editor
+            },
+            {
+                path: 'draft/:id',
+                component: Editor
+            },
+        ]
     },
     /*需要重定向的路由(必须放在后面)放前面报错*/
-    {
-        path: "/editor/draft",
-        to: "/editor/draft/new"
-    },
+
     {
         path: '/editor',
         to: "/editor/drafts"
-    },
+    }
 ];
 
 /*路由配置*/
 export const RouteWithSubRoutes = (route) => {
+
     if (route.to) {
-        return <Redirect from={route.path} to={route.to}/>
+        return <Redirect exact={true} strict={true} from={route.path} to={route.to}/>
     }
-    return <Route exact={true} strict={true} path={route.path} render={props => {
-        return <route.component {...props} routes={route.routes} render={props => {
+
+    return <Route path={route.path} render={props => {
+        console.log(props)
+        return <route.component {...props} routes={props.routes} render={props => {
             if (props.routes) {
                 props.routes.map((route, i) => (
                     <RouteWithSubRoutes key={i} {...route}/>
